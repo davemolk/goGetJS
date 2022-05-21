44359977532236b407463b07d8a54ff2c183664d
davemolk <davemolk@gmail.com>
2022-05-20 20:41:02 -0600

    get random ua

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
 	"time"
 
@@ -16,7 +17,10 @@ func makeRequest(url string, client *http.Client) (*http.Response, error) {
 	if err != nil {
 		return nil, fmt.Errorf("unable to create request: %w", err)
 	}
-	// req.Header.Set("User-Agent", uAgent)
+	
+	ua := randomUA()
+	req.Header.Set("User-Agent", ua)
+
 	res, err := client.Do(req)
 	if err != nil {
 		return nil, err
@@ -52,6 +56,26 @@ func assertErrorToNilf(msg string, err error) {
 		log.Fatalf(msg, err)
 	}
 }
+
+func randomUA() string {
+	userAgents := getUA()
+	r := rand.New(rand.NewSource(time.Now().Unix()))
+	rando := r.Intn(len(userAgents))
+
+	return userAgents[rando]
+}
+
+func getUA() []string {
+	return []string{
+		"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36",
+		"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/603.3.8 (KHTML, like Gecko)",
+		"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/601.7.7 (KHTML, like Gecko) Version/9.1.2 Safari/601.7.7",
+		"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36",
+		"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.54 Safari/537.36",
+		"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:99.0) Gecko/20100101 Firefox/99.0",
+	}
+}
+
 
 func main() {
 	url := flag.String("url", "https://go.dev/", "url to get JavaScript from")
