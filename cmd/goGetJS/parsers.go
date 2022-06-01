60c93693ced3253279edb5ab9e30cbb98b1091a6
davemolk <davemolk@gmail.com>
2022-06-01 07:45:20 -0600

    use errgroup for handling file input

--- cmd/goGetJS/parsers.go
+++ cmd/goGetJS/parsers.go
@@ -11,6 +11,7 @@ import (
 	"strings"
 
 	"github.com/PuerkitoBio/goquery"
+	"golang.org/x/sync/errgroup"
 )
 
 func parseDoc(r io.Reader, baseURL string) ([]string, int, error) {
@@ -23,7 +24,7 @@ func parseDoc(r io.Reader, baseURL string) ([]string, int, error) {
 	anonCount := 0
 
 	doc.Find("script").Each(func(i int, s *goquery.Selection) {
-		// scripts with src
+		// handling scripts with src
 		if value, ok := s.Attr("src"); ok {
 			if !strings.HasPrefix(value, "http") {
 				if !strings.HasPrefix(value, "/") {
@@ -34,10 +35,10 @@ func parseDoc(r io.Reader, baseURL string) ([]string, int, error) {
 				scriptsSRC = append(scriptsSRC, value)
 			}
 		} else {
-			// scripts without src
+			// handling scripts without src
 			script := strings.TrimSpace(s.Text())
 
-			// write to file
+			// write scripts to file
 			scriptByte := []byte(script)
 			anonCount++
 			scriptName := fmt.Sprintf("anon%s.js", strconv.Itoa(anonCount))
@@ -56,7 +57,7 @@ func parseDoc(r io.Reader, baseURL string) ([]string, int, error) {
 }
 
 func getJS(client *http.Client, url string, query interface{}, r *regexp.Regexp) error {
-	log.Println("getting script at:", url)
+	log.Println("getting javascript from:", url)
 	res, err := makeRequest(url, client)
 	if err != nil {
 		return fmt.Errorf("could not make script request: %v", err)
@@ -68,8 +69,6 @@ func getJS(client *http.Client, url string, query interface{}, r *regexp.Regexp)
 		return fmt.Errorf("unable to parse script page: %q", err)
 	}
 	script := doc.Find("body").Text()
-	currentURL := *res.Request.URL
-	url = currentURL.String()
 
 	switch q := query.(type) {
 	case *regexp.Regexp:
@@ -98,24 +97,33 @@ func getJS(client *http.Client, url string, query interface{}, r *regexp.Regexp)
 			return nil
 		}
 	case []string:
-		log.Println("file input as a slice")
-		// use goroutines here
-		for _, v := range q {
-			if strings.Contains(script, v) {
-				fmt.Printf("\nFound %q in %s\n", v, url)
-			}
-			if script != "" {
-				err := writeScripts(script, url, r)
-				if err != nil {
-					return fmt.Errorf("unable to write script file: %q", err)
+		var g errgroup.Group
+		for _, term := range q {
+			t := term
+			g.Go(func() error {
+				log.Printf("searching %s for %q\n", url, t)
+				if strings.Contains(script, t) {
+					fmt.Printf("\nFound %q in %s\n", t, url)
+				}
+				if script != "" {
+					err := writeScripts(script, url, r)
+					if err != nil {
+						return fmt.Errorf("unable to write script file for %s: %v", url, err)
+					}
+					return nil
 				}
 				return nil
-			}
+			})
 		}
+		err := g.Wait()
+		if err != nil {
+			fmt.Printf("error during search: %v\n", err)
+		}
+		fmt.Println("search completed")
+		return nil
 
 	default:
 		return fmt.Errorf("malformed query: %v", err)
-
 	}
 
 	return fmt.Errorf("no scripts at %v", url)
